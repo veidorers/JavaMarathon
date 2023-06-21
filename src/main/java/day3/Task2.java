d9f18c07343960bb1f9c94de97529ff50f333e26
Sergey Fomenko <artem.work749@gmail.com>
2023-06-21 12:37:37 +0300

    day3 solutions

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while(true) {
+            double dividend = scanner.nextDouble();
+            double divider = scanner.nextDouble();
+
+            if(divider == 0) break;
+
+            System.out.println(dividend / divider);
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -4,6 +4,18 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        for(int i = 0; i < 5; i++) {
+            double dividend = scanner.nextDouble();
+            double divider = scanner.nextDouble();
+
+            if(divider == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+
+            System.out.println(dividend / divider);
+        }
     }
 }
