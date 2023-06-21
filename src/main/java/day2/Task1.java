98607c2fe28fb63b6155df608894b2fd10b5da91
Sergey Fomenko <artem.work749@gmail.com>
2023-06-21 12:00:57 +0300

    day2 solutions

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int levels = scanner.nextInt();
 
+        if(levels >= 1 && levels <= 4)
+            System.out.println("Малоэтажный дом");
+        else if(levels >= 5 && levels <= 8)
+            System.out.println("Среднеэтажный дом");
+        else if(levels >= 9)
+            System.out.println("Многоэтажный дом");
+        else if(levels < 0)
+            System.out.println("Ошибка ввода");
     }
 }
--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -4,7 +4,19 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
 
+        if(a >= b)
+            System.out.println("Некорректный ввод");
+        else {
+            for(int i = a + 1; i < b; i++) {
+                if(i % 5 == 0 && i % 10 != 0) {
+                    System.out.println(i);
+                }
+            }
+        }
     }
 }
 
